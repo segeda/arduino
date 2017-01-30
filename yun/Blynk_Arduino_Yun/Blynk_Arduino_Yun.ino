14476c90a4232356eef873ea90dc4881ece855cd
Petr Severa <segedacz@gmail.com>
2017-01-30 22:09:36 +0100

    added MQTT

--- yun/Blynk_Arduino_Yun/Blynk_Arduino_Yun.ino
+++ yun/Blynk_Arduino_Yun/Blynk_Arduino_Yun.ino
@@ -1,8 +1,22 @@
-#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <Process.h>
 #include <Bridge.h>
+#include <BridgeClient.h>
+
+Process date;
+int32_t whenTime;
+int32_t lastTime = -1;
+BridgeClient client;
+
 #include <BlynkSimpleYun.h>
 
+#define VPIN_HUMIDITY V1
+#define VPIN_TEMP V2
+
+char auth[] = "<AUTH-KEY>"; 
+
 #include <SimpleTimer.h>
+
+SimpleTimer timer;
 
 #include <DHT.h>
 #include <DHT_U.h>
@@ -11,26 +25,40 @@
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
-char auth[] = "<AUTH-KEY>";
-
-SimpleTimer timer;
-
 DHT dht(DHTPIN, DHTTYPE);
 
-#define VPIN_HUMIDITY V1
-#define VPIN_TEMP V2
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define AIO_SERVER      "<MQTT-SERVER>"
+#define AIO_SERVERPORT  1883
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT);
+Adafruit_MQTT_Publish when = Adafruit_MQTT_Publish(&mqtt, "arduino/yun/time");
+Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, "arduino/yun/humidity");
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, "arduino/yun/temperature");
+Adafruit_MQTT_Publish heatIndex = Adafruit_MQTT_Publish(&mqtt, "arduino/yun/heatIndex");
 
 void setup()
 {
-  Serial.begin(9600);
   dht.begin();
   delay(10);
   Blynk.begin(auth);
+  Bridge.begin();
+
+  // run an initial date process. Should return:
+  // seconds since 1970-01-01 00:00:00 UTC.
+  if (!date.running()) {
+    date.begin("date");
+    date.addParameter("+%s");
+    date.run();
+  }
+
   // Setup a function to be called every second
-  timer.setInterval(10000L, sendUptime);
+  timer.setInterval(2000L, sendValues);
 }
 
-void sendUptime()
+void sendValues()
 {
   //Read the Temp and Humidity from DHT
   float h = dht.readHumidity();
@@ -38,16 +66,58 @@ void sendUptime()
   if (isnan(h) || isnan(t)) {
     return;
   }
+
+  if (lastTime != whenTime) { // if a second has passed
+    when.publish(whenTime);
+
+    // restart the date process:
+    if (!date.running()) {
+      date.begin("date");
+      date.addParameter("+%s");
+      date.run();
+    }
+  }
   
   float hic = dht.computeHeatIndex(t, h, false);
-  Blynk.virtualWrite(1, h);
-  Blynk.virtualWrite(2, t);
+  Blynk.virtualWrite(1, (int)h);
+  Blynk.virtualWrite(2, (int)t);
   Blynk.virtualWrite(3, hic);
+
+  humidity.publish((int32_t)h);
+  temperature.publish((int32_t)t);
+  heatIndex.publish(hic);
+
+  //if there's a result from the date process, parse it:
+  while (date.available() > 0) {
+    String timeString = date.readString();
+    lastTime = whenTime;
+    whenTime = timeString.toInt();
+  }
 }
 
 void loop()
 {
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
   Blynk.run();
   timer.run();
 }
 
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+  }
+}
